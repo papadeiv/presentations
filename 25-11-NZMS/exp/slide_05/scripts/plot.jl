3d410a2f250b5feed666199b554b8596f1f7853b
Davide Papapicco <davidepapapicco93@gmail.com>
2025-11-27 10:45:28 +1300

    Final version of the slides for NZMS 25

--- 25-11-NZMS/exp/slide_05/scripts/plot.jl
+++ 25-11-NZMS/exp/slide_05/scripts/plot.jl
@@ -12,7 +12,7 @@ function plot(time, state, termination)
         # Check if the timeseries has hit the target at the termination time
         if abs(state[end] - termination) < 0.01 
                 # Plot the timeseries
-                lines!(ax1, time, state, color = (:black, 0.15), linewidth = 1.0)
+                lines!(ax1, time, state, color = (:black, 0.5), linewidth = 1.0)
         end
 end
 
@@ -24,14 +24,14 @@ function plot(termination::Float64, total_time::Float64)
         time = collect(LinRange(0,T,5000))
 
         # Plot the hitting target
-        lines!(ax1, [0,T], [Y, Y], color = :black, linewidth = 5.0, linestyle = :dash)
+        lines!(ax1, [0,T], [Y, Y], color = :black, linewidth = 6.0, linestyle = :dash)
 
         # Plot the minimiser of the action
-        lines!(ax2, [0,T], [Y^2, Y^2], color = :black, linewidth = 5.0, linestyle = :dash)
+        lines!(ax2, [0,T], [Y^2, Y^2], color = :black, linewidth = 6.0, linestyle = :dash)
 
         # Plot the full paths 
-        lines!(ax1, time, [ɸ1(t) for t in time], color = CtpRed, linewidth = 5.0)
-        lines!(ax1, time, [ɸ2(t) for t in time], color = CtpBlue, linewidth = 5.0)
+        lines!(ax1, time, [ɸ1(t) for t in time], color = CtpRed, linewidth = 8.0)
+        lines!(ax1, time, [ɸ2(t) for t in time], color = CtpBlue, linewidth = 8.0)
 
         # Plot the LDP of the paths
         time = collect(LinRange(0,4,5000))
